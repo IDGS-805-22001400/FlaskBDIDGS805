2d53e69552728ef8ac240c1c1e8224ae1df540d9
IDGS-805-22001400 <thehumbertogamer2005@gmail.com>
2026-02-17 22:13:51 -0600

    insertar 17/02/26

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
     SECRET_KEY  ="ClaveSecreta"
     SESSION_COOKIE_SECURE=False
     
+    
 class DevelopmentConfig(Config):
     DEBUG=True
     SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@127.0.0.1/bdidgs805'
