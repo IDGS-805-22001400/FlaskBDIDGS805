615d7ce4066ec853fbdf7c89bdf659aab04fadd7
IDGS-805-22001400 <thehumbertogamer2005@gmail.com>
2026-02-20 20:53:45 -0600

    detalles 20/02/26

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf.csrf import CSRFProtect
-#from flask_migrate import Migrate
 from config import DevelopmentConfig
 import forms
 
@@ -9,32 +8,43 @@ from models import db, Alumnos
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db.init_app(app)
-csrf=CSRFProtect()
+csrf = CSRFProtect()
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"),404
 
-@app.route("/",methods=['GET','POST'])
+@app.route("/", methods = ['GET', 'POST'])
 @app.route("/index")
 def index():
-    create_form=forms.UserForm(request.form)
-    alumno=Alumnos.query.all()
-    return render_template("index.html",form=create_form,alumno=alumno)
+		create_form = forms.UserForm(request.form)
+		alumno = Alumnos.query.all()
+		return render_template("index.html", form = create_form, alumno = alumno)
 
-@app.route("/Alumnos",methods=['GET','POST'])
+@app.route("/Alumnos", methods = ['GET', 'POST'])
 def alumnos():
-    create_form=forms.UserForm(request.form)
-    if request.method=='POST':
-        alum=Alumnos(nombre=create_form.nombre.data,
-                     apaterno=create_form.apaterno.data,
-                     email=create_form.email.data)
-        db.session.add(alum)
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template("Alumnos.html",form=create_form)
+			create_form = forms.UserForm(request.form)
+			if request.method == 'POST':
+				alum = Alumnos(nombre = create_form.nombre.data,
+					apaterno = create_form.apaterno.data,
+					email = create_form.email.data)
+				db.session.add(alum)
+				db.session.commit()
+				return redirect(url_for('index'))
+			return render_template("Alumnos.html", form = create_form)
+
+@app.route("/detalles", methods = ['GET', 'POST'])
+def detalles():
+    if request.method=='GET':
+        id=request.args.get('id')
+        #select * from alumnos where id=id
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        nombre=alum1.nombre
+        apaterno=alum1.apaterno
+        email=alum1.email
+    return render_template('detalles.html',id=id,nombre=nombre,apaterno=apaterno,email=email)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run()
+app.run()
