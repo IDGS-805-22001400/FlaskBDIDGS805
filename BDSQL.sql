78f3baec458e834878dbf04672117e7427c30b36
IDGS-805-22001400 <thehumbertogamer2005@gmail.com>
2026-02-24 21:13:05 -0600

    migraciones 24/02/26

---
+++ BDSQL.sql
@@ -0,0 +1,78 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bdidgs805
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alembic_version`
+--
+
+DROP TABLE IF EXISTS `alembic_version`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL,
+  PRIMARY KEY (`version_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alembic_version`
+--
+
+LOCK TABLES `alembic_version` WRITE;
+/*!40000 ALTER TABLE `alembic_version` DISABLE KEYS */;
+INSERT INTO `alembic_version` VALUES ('eeb5e5b06a9e');
+/*!40000 ALTER TABLE `alembic_version` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `alumnos`
+--
+
+DROP TABLE IF EXISTS `alumnos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alumnos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `email` varchar(120) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `apellidos` varchar(200) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alumnos`
+--
+
+LOCK TABLES `alumnos` WRITE;
+/*!40000 ALTER TABLE `alumnos` DISABLE KEYS */;
+INSERT INTO `alumnos` VALUES (4,'Humberto','asdaaaa@gmail.com','2026-02-17 20:59:22','Chavez','4772299918'),(5,'Humberto','asdaaaa@gmail.com','2026-02-17 20:59:35','Chavez','4772299918'),(6,'Humberto','asdaaaa@gmail.com','2026-02-17 20:59:59','Chavez','4772299918'),(7,'Humberto','asdaaaa@gmail.com','2026-02-17 21:00:02','Chavez','4772299918'),(8,'Humberto','saddaads@gmail.com','2026-02-17 21:00:25','Chavez','4772299918'),(9,'Pedro','asds@gmail.com','2026-02-17 21:01:26','Chavez','4772299918'),(10,'Pablo','asddas@gmail.com','2026-02-17 21:02:23','Chavez','4772299918'),(11,'Humberto','thehumbertogamer2005@gmail.com','2026-02-17 21:07:25','Chavez','4772299918'),(12,'Humberto','thehumbertogamer2005@gmail.com','2026-02-17 21:09:15','Chavez','4772299918'),(13,'Humberto Chávez Cast','thehumbertogamer2005@gmail.com','2026-02-17 21:11:27','Chavez','4772299918'),(14,'Paola','asxxx@gmail.com','2026-02-17 21:57:55','Chavez','4772299918'),(15,'Fernanda','dsd@gmail.com','2026-02-17 22:05:31','Chavez','4772299918');
+/*!40000 ALTER TABLE `alumnos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-24 21:06:19
--- forms.py
+++ forms.py
@@ -1,19 +1,16 @@
-from wtforms import Form
-from wtforms import IntegerField, StringField, PasswordField
-from wtforms import EmailField
-from wtforms import validators
+from flask_wtf import FlaskForm 
+from wtforms import StringField, IntegerField, EmailField, validators
 
-
-class UserForm(Form):
-    id=IntegerField('id')
-    nombre=StringField("Nombre", [
-        validators.DataRequired(message="El campo es requerido"),
-        validators.length(min=4, max=20, message="Ingrese nombre valido")
+class UserForm(FlaskForm): 
+    id = IntegerField('ID') 
+    nombre = StringField('Nombre', [
+        validators.DataRequired(message='El nombre es requerido')
     ])
-    apaterno=StringField("Apaterno", [
-        validators.DataRequired(message="El campo es requerido")
+    apellidos = StringField('Apellidos', [
+        validators.DataRequired(message='Los apellidos son requeridos')
     ])
-    email=EmailField("Correo", [
-        validators.DataRequired(message='El correo es requerido'),
-        validators.Email(message="Ingresa correo valido")
+    email = EmailField('Correo Electrónico', [
+        validators.DataRequired(message='El email es requerido'),
+        validators.Email(message='Ingrese un correo válido')
     ])
+    telefono = StringField('Teléfono')
